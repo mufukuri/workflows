89a017dd7fce16e820d56a89514d41bc8bf6d0fa
Douglas Shingirayi Chifetete <dschifetete@gmail.com>
2016-05-02 10:49:02 +0200

    Installed gulp and gutil

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+/**
+ * Created by douglas on 5/2/16.
+ */
+var gulp =      require('gulp'),
+    gutil =     require('gulp-util');
+
+gulp.task('log',function(){
+        gutil.log("Thie really works");
+})
